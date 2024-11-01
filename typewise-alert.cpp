a24ae5d2e544a9604c2c24ef05b35e339ab4db15
sriganesh12 <49653366+sriganesh12@users.noreply.github.com>
2024-11-01 18:19:02 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -1,7 +1,7 @@
 #include "typewise-alert.h"
 #include <stdio.h>
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
+TypewiseAlert::BreachType TypewiseAlert::inferBreach(double value, double lowerLimit, double upperLimit) {
   if(value < lowerLimit) {
     return TOO_LOW;
   }
@@ -14,8 +14,7 @@ BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
   }
 }
 
-BreachType classifyTemperatureBreach(
-    CoolingType coolingType, double temperatureInC) {
+TypewiseAlert::BreachType TypewiseAlert::classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
   int lowerLimit = 0;
   int upperLimit = 0;
   if (coolingType == PASSIVE_COOLING) 
@@ -33,9 +32,7 @@ BreachType classifyTemperatureBreach(
   return inferBreach(temperatureInC, lowerLimit, upperLimit);
 }
 
-void checkAndAlert(
-    AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
-
+void TypewiseAlert::checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
   BreachType breachType = classifyTemperatureBreach(
     batteryChar.coolingType, temperatureInC
   );
@@ -50,12 +47,12 @@ void checkAndAlert(
   }
 }
 
-void sendToController(BreachType breachType) {
+void TypewiseAlert::sendToController(BreachType breachType) {
   const unsigned short header = 0xfeed;
   printf("%x : %x\n", header, breachType);
 }
 
-void sendToEmail(BreachType breachType) {
+void TypewiseAlert::sendToEmail(BreachType breachType) {
   const char* recepient = "a.b@c.com";
   if (breachType == TOO_LOW)
   {
